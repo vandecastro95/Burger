084414223b63969b8c20beeaefe360f41ded2c7d
unknown <vandecastro95@gmail.com>
2018-11-07 17:18:39 -0600

    created mysql db

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS `burgers_db`;
+CREATE DATABASE `burgers_db`;
+
+
+CREATE TABLE `burgers_db`.`burgers` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `burger_name` VARCHAR(45) NOT NULL,
+  `devoured` TINYINT NOT NULL,
+  PRIMARY KEY (`id`));
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Poblano Picasso Burger (Comes with Poblano)", False);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Foot Feta-Ish Burger (Comes with Feta Cheese)", False);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Child Molester  (Comes With Candy!)", False);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Baby You Can Chive My Car Burger (Comes with Chives)", False);
